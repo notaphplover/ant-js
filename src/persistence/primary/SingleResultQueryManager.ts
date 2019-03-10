295f1d4eecc6e9e57a5e468260bf1fcc47b64836
Roberto Pintos López <roberto@secture.com>
2019-03-10 13:39:38 +0100

    Query managers have a public model property.

--- src/persistence/primary/SingleResultQueryManager.ts
+++ src/persistence/primary/SingleResultQueryManager.ts
@@ -21,7 +21,7 @@ export abstract class SingleResultQueryManager<
       this._luaDeleteGenerator(),
       1,
       this._reverseHashKey,
-      JSON.stringify(entity[this._primaryModelManager.model.id]),
+      JSON.stringify(entity[this.model.id]),
       JSON.stringify(VOID_RESULT_STRING),
     );
   }
@@ -76,7 +76,7 @@ export abstract class SingleResultQueryManager<
       2,
       this._reverseHashKey,
       this._key(entity),
-      JSON.stringify(entity[this._primaryModelManager.model.id]),
+      JSON.stringify(entity[this.model.id]),
     );
   }
 
