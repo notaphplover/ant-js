1b96dd0b8a8dac007231740cc83a1ed33167cab5
Roberto Pintos López <roberto@secture.com>
2019-04-04 23:39:10 +0200

    Fixed entity manager interfaces.

--- src/persistence/secondary/ISecondaryEntityManager.ts
+++ src/persistence/secondary/ISecondaryEntityManager.ts
@@ -1,6 +1,23 @@
 import { IEntity } from '../../model/IEntity';
-import { IEntityManager } from '../IEntityManager';
+import { IModel } from '../../model/IModel';
 
-export interface ISecondaryEntityManager<TEntity extends IEntity>
-  extends IEntityManager<TEntity> {
+export interface ISecondaryEntityManager<TEntity extends IEntity> {
+
+  /**
+   * Model of the manager.
+   */
+  model: IModel;
+  /**
+   * Gets a model by its id.
+   * @param id: Model's id.
+   * @returns Model found.
+   */
+  getById(id: number|string): Promise<TEntity>;
+
+  /**
+   * Gets a collection of models by its ids.
+   * @param ids Model ids.
+   * @returns Models found.
+   */
+  getByIds(ids: number[]| string[]): Promise<TEntity[]>;
 }
