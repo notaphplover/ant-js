a59c723e63bb856f77eb3c88baf6cf6ae6b5a681
Roberto Pintos López <roberto@secture.com>
2019-05-15 22:10:38 +0200

    Added negative cache option to the model config.

--- src/api/config/IAntModelConfig.ts
+++ src/api/config/IAntModelConfig.ts
@@ -2,6 +2,10 @@ import * as IORedis from 'ioredis';
 
 export interface IAntModelConfig {
   /**
+   * True to perform a negative entity cache strategy.
+   */
+  negativeCache?: boolean;
+  /**
    * Redis connection.
    */
   redis: IORedis.Redis;
