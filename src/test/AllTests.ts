90fed950d8f7c8e546ef7b1c7185461453a6e763
Roberto Pintos López <roberto@secture.com>
2019-06-27 21:15:14 +0200

    Added RedisCachedScriptSet.

--- src/test/AllTests.ts
+++ src/test/AllTests.ts
@@ -9,6 +9,7 @@ import { PrimaryEntityManagerTest } from './primary/PrimaryEntityManagerTest';
 import { MultipleResultQueryManagerTest } from './primary/query/MultipleResultQueryManagerTest';
 import { SingleResultQueryManagerTest } from './primary/query/SingleResultQueryManagerTest';
 import { RedisWrapper } from './primary/RedisWrapper';
+import { RedisCachedScriptSetByCacheModeTest } from './primary/script/RedisCachedScriptSetByCacheModeTest';
 import { RedisCachedScriptTest } from './primary/script/RedisCachedScriptTest';
 
 export class AllTest implements ITest {
@@ -29,6 +30,7 @@ export class AllTest implements ITest {
     new ModelTest().performTests();
     new MultipleResultQueryManagerTest(beforeAllPromise).performTests();
     new PrimaryEntityManagerTest(beforeAllPromise).performTests();
+    new RedisCachedScriptSetByCacheModeTest(beforeAllPromise).performTests();
     new RedisCachedScriptTest(beforeAllPromise).performTests();
     new SingleResultQueryManagerTest(beforeAllPromise).performTests();
   }
