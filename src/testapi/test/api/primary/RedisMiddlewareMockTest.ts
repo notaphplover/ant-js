5a092dc7b89a9a7d7904b49d461747e903770900
Roberto Pintos López <roberto@secture.com>
2019-08-06 20:24:36 +0200

    Added RedisMiddlewareMock.

---
+++ src/testapi/test/api/primary/RedisMiddlewareMockTest.ts
@@ -0,0 +1,165 @@
+import { IEntity } from '../../../../model/IEntity';
+import { Model } from '../../../../model/Model';
+import { ModelManagerGenerator } from '../../../../test/primary/ModelManagerGenerator';
+import { SecondaryEntityManagerMock } from '../../../../test/secondary/SecondaryEntityManagerMock';
+import { ITest } from '../../../api/ITest';
+import { RedisMiddlewareMock } from '../../../api/primary/RedisMiddlewareMock';
+
+interface IEntityTest extends IEntity {
+  id: number;
+  numberField: number;
+  strField: string;
+}
+
+const modelTestProperties = ['id', 'numberField', 'strField'];
+const modelTestGenerator = (prefix: string) => new Model('id', {prefix: prefix});
+
+export class RedisMiddlewareMockTest implements ITest {
+
+  /**
+   * Model manager generator.
+   */
+  protected _modelManagerGenerator: ModelManagerGenerator<IEntityTest>;
+
+  public constructor() {
+    this._modelManagerGenerator = new ModelManagerGenerator();
+  }
+
+  public performTests(): void {
+    describe(RedisMiddlewareMockTest.name, () => {
+      this._itMustBeInitializable();
+      this._itMustNotBreakAModelManagerDelete();
+      this._itMustNotBreakAModelManagerMDelete();
+      this._itMustNotBreakAModelManagerMUpdate();
+      this._itMustNotBreakAModelManagerUpdate();
+    });
+  }
+
+  private _itMustBeInitializable(): void {
+    it('It must be initializable', async (done) => {
+      expect(() => {
+        // tslint:disable-next-line:no-unused-expression
+        new RedisMiddlewareMock();
+      }).not.toThrowError();
+      done();
+    });
+  }
+
+  private _itMustNotBreakAModelManagerDelete(): void {
+    const itsName = 'It must not break a model manager delete';
+    const prefix = RedisMiddlewareMockTest.name + '/' + itsName + '/';
+    it(itsName, async (done) => {
+      const model = modelTestGenerator(prefix);
+      const entityToDelete: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'one',
+      };
+      const anotherEntity = {
+        id: 1,
+        numberField: 2,
+        strField: 'two',
+      };
+      const [modelManager] = this._modelManagerGenerator.generateModelManager(
+        model,
+        modelTestProperties,
+        prefix + 'query/',
+        prefix + 'reverse/',
+        new SecondaryEntityManagerMock(model, [anotherEntity]),
+        true,
+        new RedisMiddlewareMock(),
+      );
+
+      await modelManager.delete(entityToDelete.id);
+      const entityFound = await modelManager.get(anotherEntity.id);
+      expect(entityFound).toEqual(anotherEntity);
+      done();
+    });
+  }
+
+  private _itMustNotBreakAModelManagerMDelete(): void {
+    const itsName = 'It must not break a model manager mDelete';
+    const prefix = RedisMiddlewareMockTest.name + '/' + itsName + '/';
+    it(itsName, async (done) => {
+      const model = modelTestGenerator(prefix);
+      const entityToDelete: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'one',
+      };
+      const anotherEntity = {
+        id: 1,
+        numberField: 2,
+        strField: 'two',
+      };
+      const [modelManager] = this._modelManagerGenerator.generateModelManager(
+        model,
+        modelTestProperties,
+        prefix + 'query/',
+        prefix + 'reverse/',
+        new SecondaryEntityManagerMock(model, [anotherEntity]),
+        true,
+        new RedisMiddlewareMock(),
+      );
+
+      await modelManager.mDelete([entityToDelete.id]);
+      const entityFound = await modelManager.get(anotherEntity.id);
+      expect(entityFound).toEqual(anotherEntity);
+      done();
+    });
+  }
+
+  private _itMustNotBreakAModelManagerMUpdate(): void {
+    const itsName = 'It must not break a model manager mUpdate';
+    const prefix = RedisMiddlewareMockTest.name + '/' + itsName + '/';
+    it(itsName, async (done) => {
+      const model = modelTestGenerator(prefix);
+      const entityToUpdate: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'one',
+      };
+      const [modelManager] = this._modelManagerGenerator.generateModelManager(
+        model,
+        modelTestProperties,
+        prefix + 'query/',
+        prefix + 'reverse/',
+        new SecondaryEntityManagerMock(model, [entityToUpdate]),
+        true,
+        new RedisMiddlewareMock(),
+      );
+
+      await modelManager.mUpdate([entityToUpdate]);
+      const entityFound = await modelManager.get(entityToUpdate.id);
+      expect(entityFound).toEqual(entityToUpdate);
+      done();
+    });
+  }
+
+  private _itMustNotBreakAModelManagerUpdate(): void {
+    const itsName = 'It must not break a model manager update';
+    const prefix = RedisMiddlewareMockTest.name + '/' + itsName + '/';
+    it(itsName, async (done) => {
+      const model = modelTestGenerator(prefix);
+      const entityToUpdate: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'one',
+      };
+      const [modelManager] = this._modelManagerGenerator.generateModelManager(
+        model,
+        modelTestProperties,
+        prefix + 'query/',
+        prefix + 'reverse/',
+        new SecondaryEntityManagerMock(model, [entityToUpdate]),
+        true,
+        new RedisMiddlewareMock(),
+      );
+
+      await modelManager.update(entityToUpdate);
+      const entityFound = await modelManager.get(entityToUpdate.id);
+      expect(entityFound).toEqual(entityToUpdate);
+      done();
+    });
+  }
+}
