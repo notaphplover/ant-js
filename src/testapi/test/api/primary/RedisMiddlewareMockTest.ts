29a0e50532e78fec2824daadc88015c307920cf1
Roberto Pintos López <roberto@secture.com>
2019-08-21 19:14:25 +0200

    Added tests.

--- src/testapi/test/api/primary/RedisMiddlewareMockTest.ts
+++ src/testapi/test/api/primary/RedisMiddlewareMockTest.ts
@@ -1,6 +1,10 @@
 import { IEntity } from '../../../../model/IEntity';
+import { IModel } from '../../../../model/IModel';
 import { Model } from '../../../../model/Model';
+import { IPrimaryEntityManager } from '../../../../persistence/primary/IPrimaryEntityManager';
+import { IPrimaryQueryManager } from '../../../../persistence/primary/query/IPrimaryQueryManager';
 import { ModelManagerGenerator } from '../../../../test/primary/ModelManagerGenerator';
+import { MultipleResultQueryByFieldManager } from '../../../../test/primary/query/MultipleResultQueryByFieldManager';
 import { SecondaryEntityManagerMock } from '../../../../test/secondary/SecondaryEntityManagerMock';
 import { ITest } from '../../../api/ITest';
 import { RedisMiddlewareMock } from '../../../api/primary/RedisMiddlewareMock';
@@ -34,7 +38,37 @@ export class RedisMiddlewareMockTest implements ITest {
       this._itMustNotBreakAModelManagerMGet();
       this._itMustNotBreakAModelManagerMUpdate();
       this._itMustNotBreakAModelManagerUpdate();
+      this._itMustNotBreakAMultipleResultQueryManagerGet();
+      this._itMustNotBreakASingleResultQueryManagerGet();
     });
+  }
+
+  /**
+   * Helper for searching entities.
+   * @param entity Entity to search.
+   * @param model Model of the entities.
+   * @param primaryEntityManager Primary entity manager.
+   * @param queriesMap Queries map.
+   * @returns Promise of entities found by the primary and the query managers.
+   */
+  private async _helperSearchEntity(
+    entity: IEntityTest,
+    model: IModel,
+    primaryEntityManager: IPrimaryEntityManager<IEntityTest>,
+    queriesMap: Map<string, IPrimaryQueryManager<IEntityTest>>,
+  ): Promise<[IEntityTest, IEntityTest[]]> {
+    const searchEntityByPrimaryEntityManager = await primaryEntityManager.get(entity[model.id]);
+    const searchEntityByQueryManager = new Array();
+    for (const [property, query] of queriesMap) {
+      const searchArgs: any = {};
+      searchArgs[property] = entity[property];
+      const result = await query.get(searchArgs);
+      searchEntityByQueryManager.push(result);
+    }
+    return [
+      searchEntityByPrimaryEntityManager,
+      searchEntityByQueryManager,
+    ];
   }
 
   private _itMustBeInitializable(): void {
@@ -213,6 +247,85 @@ export class RedisMiddlewareMockTest implements ITest {
       await modelManager.update(entityToUpdate);
       const entityFound = await modelManager.get(entityToUpdate.id);
       expect(entityFound).toEqual(entityToUpdate);
+      done();
+    });
+  }
+
+  private _itMustNotBreakAMultipleResultQueryManagerGet(): void {
+    const itsName = '';
+    const prefix = '';
+    it(itsName, async (done) => {
+      const entity1: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'a',
+      };
+      const entities: IEntityTest[] = [
+        entity1,
+      ];
+      const model = modelTestGenerator(prefix);
+      const secondaryEntityManager = new SecondaryEntityManagerMock<IEntityTest>(
+        model,
+        entities,
+      );
+      const [
+        modelManager,
+        primaryEntityManager,
+      ] = this._modelManagerGenerator.generateZeroQueriesModelManager(
+        model,
+        secondaryEntityManager,
+      );
+      const query = new MultipleResultQueryByFieldManager(
+        (params: any) =>
+          new Promise((resolve) => {
+            const entities = secondaryEntityManager.store.filter(
+              (entity) => params.strField === entity.strField,
+            );
+            resolve(entities.map((entity) => entity.id));
+          }),
+        primaryEntityManager,
+        new RedisMiddlewareMock(),
+        prefix + 'reverse/',
+        'strField',
+        prefix + 'query/',
+      );
+      modelManager.addQuery(query);
+      await query.get(entity1);
+      const [ entityFound ] = await query.get(entity1);
+      expect(entityFound).toEqual(entity1);
+      done();
+    });
+  }
+
+  private _itMustNotBreakASingleResultQueryManagerGet(): void {
+    const itsName = 'It must not break a single result query manager get';
+    const prefix = RedisMiddlewareMockTest.name + '/' + itsName + '/';
+    it(itsName, async (done) => {
+      const model = modelTestGenerator(prefix);
+      const entity: IEntityTest = {
+        id: 0,
+        numberField: 1,
+        strField: 'one',
+      };
+      const [, primaryEntityManager , queryManagersByProperty] = this._modelManagerGenerator.generateModelManager(
+        model,
+        modelTestProperties,
+        prefix + 'query/',
+        prefix + 'reverse/',
+        new SecondaryEntityManagerMock(model, [entity]),
+        true,
+        new RedisMiddlewareMock(),
+      );
+
+      const [
+        searchEntityByPrimaryEntityManager,
+        searchEntityByQueryManager,
+      ] = await this._helperSearchEntity(entity, model, primaryEntityManager, queryManagersByProperty);
+
+      expect(searchEntityByPrimaryEntityManager).toEqual(entity);
+      for (const search of searchEntityByQueryManager) {
+        expect(search).toEqual(entity);
+      }
       done();
     });
   }
