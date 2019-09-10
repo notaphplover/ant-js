3f6789ada1723cd9d046beea8f3e80bafa469e40
Roberto Pintos López <roberto@secture.com>
2019-09-10 18:48:18 +0200

    Added doc and renamed method.

--- src/persistence/primary/script/RedisCachedScriptSetByCacheMode.ts
+++ src/persistence/primary/script/RedisCachedScriptSetByCacheMode.ts
@@ -34,15 +34,17 @@ export class RedisCachedScriptSetByCacheMode implements IRedisCachedScriptSet<IP
     }
     const index = gArgs.ttl ? 1 : 0;
     if (null == scripts[index]) {
-      scripts[index] = this._generateCachedScript(gArgs);
+      scripts[index] = this._generateScript(gArgs);
     }
     return scripts[index].eval(eArgsGen);
   }
 
   /**
-   * @inheritdoc
+   * Generates an script with the given persistency options.
+   * @param options Persistency options.
+   * @returns Generated script.
    */
-  protected _generateCachedScript(options: IPersistencyUpdateOptions): RedisCachedScript {
+  protected _generateScript(options: IPersistencyUpdateOptions): RedisCachedScript {
     return this._generator(options);
   }
 }
