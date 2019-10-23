a978042c015a46931056dfbaa358976afdcb7b5a
Roberto Pintos López <roberto@secture.com>
2019-10-23 11:19:42 +0200

    Added common interfaces to main script.

--- src/ant.ts
+++ src/ant.ts
@@ -1,3 +1,35 @@
 import { AntManager } from './api/ant-manager';
+import { ApiGeneralManager } from './api/api-general-manager';
+import { ApiGeneralConfig } from './api/config/api-general-config';
+import { ApiModelConfig } from './api/config/api-model-config';
+import { ApiQueryConfig } from './api/config/api-query-config';
+import { ApiMultipleResultQueryManager } from './api/query/api-multiple-result-query-manager';
+import { ApiQueryManager } from './api/query/api-query-manager';
+import { ApiSingleResultQueryManager } from './api/query/api-single-result-query-manager';
+import { AntModel } from './model/ant-model';
+import { Entity } from './model/entity';
+import { KeyGenParams } from './model/key-gen-params';
+import { Model } from './model/model';
+import { CacheMode } from './persistence/primary/options/cache-mode';
+import { PersistencyDeleteOptions } from './persistence/primary/options/persistency-delete-options';
+import { PersistencySearchOptions } from './persistence/primary/options/persistency-search-options';
+import { PersistencyUpdateOptions } from './persistence/primary/options/persistency-update-options';
 
-export { AntManager };
+export {
+  AntManager,
+  AntModel,
+  ApiGeneralConfig,
+  ApiGeneralManager,
+  ApiModelConfig,
+  ApiMultipleResultQueryManager,
+  ApiQueryConfig,
+  ApiQueryManager,
+  ApiSingleResultQueryManager,
+  CacheMode,
+  Entity,
+  KeyGenParams,
+  Model,
+  PersistencyDeleteOptions,
+  PersistencySearchOptions,
+  PersistencyUpdateOptions,
+};
